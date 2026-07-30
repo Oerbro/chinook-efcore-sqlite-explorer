2de47d51edb01c0cdb3d1d9841111a915e1ee797
Oerbro <129538231+Oerbro@users.noreply.github.com>
2026-07-30 14:47:19 +0200

    feat(data): scaffold Chinook EF Core model

---
+++ ChinookExplorer.Data/Models/InvoiceLine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChinookExplorer.Data.Models;
+
+public partial class InvoiceLine
+{
+    public int InvoiceLineId { get; set; }
+
+    public int InvoiceId { get; set; }
+
+    public int TrackId { get; set; }
+
+    public double UnitPrice { get; set; }
+
+    public int Quantity { get; set; }
+
+    public virtual Invoice Invoice { get; set; } = null!;
+
+    public virtual Track Track { get; set; } = null!;
+}
